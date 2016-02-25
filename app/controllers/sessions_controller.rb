7ad81a2e21c6ff4914d0eb9b2b28310e43714370
Kevin McCormack <wittyman37@yahoo.com>
2016-02-25 09:29:57 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    #binding.pry
+    user = User.find_or_create_by(uid: auth['uid']) do |u|
+
+      u.name = auth[:info][:name]
+    end
+    session[:user_id] = user.id
+    redirect_to '/'
+  end
+
+  def auth
+    request.env['omniauth.auth']
+  end
+end
