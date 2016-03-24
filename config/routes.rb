7efcf68c0f5b03e648355271d53c7491d7e8c3df
Joseph Surla <josephsurla@gmail.com>
2016-03-24 15:18:49 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/auth/facebook/callback', to: 'sessions#create'
+
+  resources :sessions
+  
   root 'welcome#home'
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
