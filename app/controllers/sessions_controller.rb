01b7e9b2d2a1598b663eb915c16201ae19498de2
Joshua Azemoh <joshazemoh@gmail.com>
2016-06-10 16:39:30 +0100

    display user details

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by_omniauth(request.env['omniauth.auth'])
+    user = User.find_or_create_by_omniauth(auth)
     session[:user_id] = user.id
+  end
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
   end
 end
