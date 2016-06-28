85ebc70b1f4c384803499ba6a6c1f0b5e704a324
vannak1 <vannak.chhay1@gmail.com>
2016-06-28 18:15:41 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  
+  resources :sessions, only: [:create, :destroy]
+  resource :welcome, only: [:home]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
