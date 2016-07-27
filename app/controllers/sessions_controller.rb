bbe26bd66a7b77bfc78c6bf52fa0240f4afc6419
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-27 21:03:43 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
-  
+
   def create
      user = User.find_or_create_by(:uid => auth['uid']) do |user|
        user.name = auth['info']['name']
+       user.email = auth['info']['email']
      end
      session[:user_id] = user.try(:id)
    end
