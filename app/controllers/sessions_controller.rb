ae4a84c263449ab905f51977688a9cd0ec761c9b
Christian Whitesides <whitesidescr@gmail.com>
2016-08-29 15:37:54 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
       u.name = auth['info']['name']
     end
     session[:user_id] = user.id
+    redirect_to root_url
   end
 
   def auth
     request.env['omniauth.auth']
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
   end
 end
