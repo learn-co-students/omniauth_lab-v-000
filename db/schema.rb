78338efdac65d45ecabc6ff8f799f9c0dcae751a
jwolfe890 <jwolfe890@gmail.com>
2016-09-10 15:46:02 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,9 @@
 ActiveRecord::Schema.define(version: 20160910185236) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.integer  "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string  "name"
+    t.string  "email"
+    t.integer "uid"
   end
 
 end
