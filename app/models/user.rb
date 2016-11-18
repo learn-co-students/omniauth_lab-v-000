699bc8d890aee3a5c6fff9a0c3be189f4042337f
williamjones62 <williamjones62@gmail.com>
2016-11-18 09:08:23 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,2 @@
 class User < ActiveRecord::Base
-  def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create.tap do |user|
-      user.provider ||= auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.save
-    end
-  end
 end
