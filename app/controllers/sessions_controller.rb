5e58c8ac133bada5ae5f4b3c9e56ba91e3e970c5
vanitanaidu <vanita112003@gmail.com>
2017-04-25 12:51:38 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,10 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.find_or_create_by(:uid => auth['uid'])
-      byebug
       user.name = auth['info']['name']
         user.save
       session[:user_id] = user.try(:id)
-
     else
-
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
