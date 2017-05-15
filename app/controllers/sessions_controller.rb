1e16e23c10ed546d7ffc98126b535493edcb6595
github username <github email address>
2017-05-15 10:22:41 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,12 @@
 class SessionsController < ApplicationController
   def create
   	@auth = auth
+  #	binding.pry
     user = User.find_or_create_by(:uid => auth['uid']) do |u|
-      u.name = auth['info']['name']
+    	u.name = auth['info']['name']
     end
+   # binding.pry
+
     session[:user_id] = user.id
   end
  
