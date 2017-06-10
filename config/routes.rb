8bd742e7c76b18fa2bbf229d9fef9585973a85da
Sergio Mendoza <smendoza787@gmail.com>
2017-06-09 23:30:51 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-
-  
+  get 'sessions/create'
+  get 'sessions/auth'
+  get '/auth/:provider/callback', to: 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
