2041985145a839b8ad1609541959c1281d375374
sarjumulmi <mulmi.sarju@gmail.com>
2017-08-04 13:21:23 -0400

    Done.

--- spec/views/home.html.erb_spec.rb
+++ spec/views/home.html.erb_spec.rb
@@ -1,8 +1,8 @@
-require "spec_helper"
+require 'rails_helper.rb'
 
-describe "welcome/home.html.erb" do
+describe "welcome/home.html.erb" , type: :view do
   it "displays all the widgets" do
-    render
+    render 
     expect(rendered).to have_link('Login with Facebook', :href => '/auth/facebook')
   end
 end
