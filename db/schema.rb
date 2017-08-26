094a862d57af60c3756c3a77a381e31608b33803
Laamia Islam <laamia.islam@gmail.com>
2017-08-26 16:36:07 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170826162958) do
+
+  create_table "users", force: :cascade do |t|
+    t.string  "email"
+    t.string  "password"
+    t.integer "uid"
+    t.string  "name"
+  end
 
 end
