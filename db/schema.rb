6d0c5a12b213b257c6a35a0b981587d1a4f38e19
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-09-22 04:40:58 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170921201010) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid"
+    t.string "name"
+  end
 
 end
