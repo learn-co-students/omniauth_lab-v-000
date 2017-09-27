b6d94b60907469f524c59de9ae00ff078f1c9207
Terry Blue <terryblue@comcast.net>
2017-09-27 22:25:35 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+    @auth = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
-    session[:user_id] = @user.id
+    session[:user_id] = @auth.id
 
     render 'welcome/home'
   end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
+
   def home
+    if session[:user_id]
+      @auth = User.find(session[:user_id])
+    end
   end
+
 end
