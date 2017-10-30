de28f21038637d94087def48d1f03354a236e2e1
Jim Stricker <jmstricker93@gmail.com>
2017-10-30 15:58:28 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def render_info(user)
+    if !user.nil?
+      # raise params.inspect
+      content_tag("p", user.name)
+      content_tag("p", user.uid)
+      content_tag("p", user.email)
+      image_tag(user.image)
+    end
+  end
 end
