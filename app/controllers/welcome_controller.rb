4dcfaf12b252744217aac7d4d05d19ca336b33b5
github username <github email address>
2017-12-18 17:43:14 -0600

    Done.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+
   def home
+    #if someone is not logged in, they should see the Facebook link
+    if session[:user_id]
+     @user = User.find_by(id: session[:user_id])
+    end
   end
+
 end
