1532a1a6f54d9f97c5d6e72360879ee3a1eb5e82
chrisjamesr <chrisjames.r@gmail.com>
2018-01-05 13:09:26 -0500

    add find_or_create method for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(:email => auth_hash["info"]["email"]).first_or_create do |user|
+      user.uid = auth_hash["info"]["uid"]
+    end  
+  end
+
+
 end
