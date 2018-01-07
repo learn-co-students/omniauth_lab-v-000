613d2f8e2cf51ad90638d814e6f807be70903d19
chrisjamesr <chrisjames.r@gmail.com>
2018-01-06 21:05:25 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'bcrypt', '~> 3.1.7'
 gem 'omniauth'
 gem 'omniauth-facebook'
+gem 'dotenv-rails'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
 
   def self.find_or_create_by_omniauth(auth_hash)
-    where(:email => auth_hash["info"]["email"]).first_or_create do |user|
-      user.uid = auth_hash["info"]["uid"]
+    where(:name => auth_hash["info"]["name"]).first_or_create do |user|
+      user.uid = auth_hash["uid"]
     end  
+    
   end
 
 
