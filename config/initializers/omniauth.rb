703219f07d2aa49a3c7ab76dc70b10be6439d583
Jacqueline <anginblu@gmail.com>
2018-02-06 18:53:22 -0500

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1788265767871902', 'dcc6f58278e45625d289d42610677d2d'
+  provider :facebook, ENV['1788265767871902'], ENV['dcc6f58278e45625d289d42610677d2d']
 end
