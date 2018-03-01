ffde18a0f14b38685633bc414bd110801a5f50e8
Richard Steinbach <steinbach.dick@gmail.com>
2018-02-28 22:13:46 -0800

    omniauth_lab-v-000

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
+#require 'pry'
 class WelcomeController < ApplicationController
   def home
+    #binding.pry
+    if session[:user_id]
+      @user = User.find_or_create_by(id: session[:user_id])
+    end
   end
 end
