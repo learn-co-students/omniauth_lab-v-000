29634ef9ab03abfb2150889b2292347de4a27eb3
Alex <github email address>
2018-03-20 12:29:21 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
-  def create
-  end
+      def create
+          #facebook will send back information that will be forwarded from the config/routes file into the create action
+          @user = User.find_or_create_by(uid: auth['uid']) do |u|
+                u.name  = auth['info']['name']
+                u.email = auth['info']['email']
+                u.image = auth['info']['image']
+          end
+          session[:user_id] = @user.id
+          render 'welcome/home'
+      end
+
+      private
+
+      def auth
+          request.env['omniauth.auth']
+      end
 end
