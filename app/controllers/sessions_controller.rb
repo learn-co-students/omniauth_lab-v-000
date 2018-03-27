d1ece9366c4bca585c61cb3bbdf7ef631119cb39
Scott Wakeman <github email address>
2018-03-26 22:17:52 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,22 @@ class SessionsController < ApplicationController
        user = User.find_or_create_by_omniauth(auth_hash)
        session[:user_id] = user.id
        redirect_to root_path
+       #byebug HITS
      else
        #Normal login with Username & Password
+       # NOT HITTING THIS byebug
        user = user.find_by(:email => params[:email])
        if user && user.authenticate(params[:password])
-         session[user_id] = user.id
+         session[:user_id] = user.id
          redirect_to root_path
        else
          render ‘sessions/new’
        end
+     end
+
+     def destroy
+       reset session
+       redirect_to login_path
      end
    end
 
