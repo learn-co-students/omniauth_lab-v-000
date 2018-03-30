2f9496b1abf7dec89fa158cf9797e109c06e692a
catalanowebdevelopment <catalano.work@gmail.com>
2018-03-30 14:05:18 -0400

    Done.

---
+++ db/migrate/20180330172515_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :name
+      t.string :image
+      t.integer :uid
+
+      t.timestamps null: false
+    end
+  end
+end
