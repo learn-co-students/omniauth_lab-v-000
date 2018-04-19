3cdf7cb5d9b2ed483f2ced9228dcfb0d4d59d580
ronald vilorio <soccer23415@gmail.com>
2018-04-19 14:54:30 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'dotenv-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry'
+  gem 'pry-byebug'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
 class SessionsController < ApplicationController
   def create
-    # ***i
-    if @auth= request.env["omniauth.auth"]
-      @user = User.find_or_create_by(@auth) do |u|
-        u.name = auth['info']['name']
-        u.email = auth['info']['email']
+
+    @auth = request.env['omniauth.auth']
+
+    if @auth
+       @user = User.find_or_create_by(uid: @auth['uid']) do |u|
+        u.provider = @auth['provider']
+        u.name = @auth['info']['name']
+        u.email = @auth['info']['email']
       end
       session[:user_id] = @user.id
+      redirect_to 'welcome/home'
+    else
+
     end
+
+
   end
-
-
 
 end
