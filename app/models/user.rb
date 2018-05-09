ce86265310fb2b19b3035c2ee50c973d5c5b7caf
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-05-09 11:02:59 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
 
 	def self.find_or_create_by_omniauth(auth)
-		where(name: auth[:info][:name]).first_or_create do |user|
+		where(uid: auth[:uid]).first_or_create do |user|
 			user.email = auth[:info][:email]
 			user.name = auth[:info][:name]
 			user.image = auth[:info][:image]
-			user.uid = auth[:info][:uid]
 		end 
 	end 
 
