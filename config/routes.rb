4c2fecaec71910054c1af3f9c0c69a40d951a01c
github username <github email address>
2018-05-16 18:56:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+  get '/omniauth/github/callback' => 'sessions#create'
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
