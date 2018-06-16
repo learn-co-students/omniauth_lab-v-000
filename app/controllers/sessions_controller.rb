f56c071d81595ae4271f3701a013657b2a6f8732
github username <github email address>
2018-06-16 18:44:12 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
 
-
   def create
     user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
@@ -8,10 +7,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.try(:id)
   end
 
-
+  private
     def auth
       request.env['omniauth.auth']
     end
-
 
 end
