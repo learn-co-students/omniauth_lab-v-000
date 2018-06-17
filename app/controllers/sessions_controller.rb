e3e2cc56827a7191217c8a323f8383b9e2fa8038
ritchey0713 <ritchey0713@gmail.com>
2018-06-17 19:36:24 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_or_create_by(uid: auth_hash['uid']) do |u|
+      u.name = auth_hash['info']['name']
+      u.email = auth_hash['info']['email']
+      u.image = auth_hash['info']['image']
+      u.provider = auth_hash['info']['provider']
+    end
+    session[:user_id] = @user.id
+    render 'welcome/home'
+  end
+
+private
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
