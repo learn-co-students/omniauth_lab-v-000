e74a3d0da75bb236c8ef2f4ad65bd5b39f8fa630
github username <github email address>
2018-06-21 12:41:27 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,12 @@
 class SessionsController < ApplicationController
     def create
-        @user = User.find_or_create_by(uid: auth[:uid]) do |u|
-        u.name = auth['info']['name']
-        u.email = auth['info']['email']
-        u.image = auth['info']['image']
-        @user.create(user_params)
-
-        session[:user_id] = @user.id
-
-        render 'welcome/home'
-
+        user = User.find_or_create_by(uid: auth[:uid]) do |u|
+            u.name = auth['info']['name']
         end
+            session[:user_id] = user.try(:id)
+
+        # render 'welcome/home'
+
     end
 
     private
