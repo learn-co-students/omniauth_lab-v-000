a1034a1b4abe481ae22562f2dcdd87597aa870ab
jebediahelliott <jebediahelliott@gmail.com>
2018-07-31 14:56:48 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      binding.pry
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
