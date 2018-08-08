91556b52e4511f9b11e3a0e0a1bc2feb04f3caf3
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-08 13:06:23 -0400

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth[:info][:name]
-      u.image = auth[:info][:image]
-      u.email = auth[:info][:email]
+      u.name = auth['info']['name']
+      u.image = auth['info']['image']
+      u.email = auth['info']['email']
     end
 
     session[:user_id] = @user.id
+
+
 
     render 'welcome/home'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  
   root 'welcome#home'
 
   get '/auth/facebook/callback' => 'sessions#create'
-  
+
 end
