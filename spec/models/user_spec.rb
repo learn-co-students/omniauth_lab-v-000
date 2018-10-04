812a0b352022c00a633e4fddbe7410ab84bf07c3
Adam Duff <j.adam.duff@gmail.com>
2018-10-04 23:11:33 +0000

    automatically backed up by learn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,1 @@
 require 'rails_helper'
-<<<<<<< HEAD
-=======
-
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
->>>>>>> 9665d99accfb815ddee419bdc8f86da7c6ced984
