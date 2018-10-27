2fb1a2924a1434a47f2ec11f36a22a6bb521998d
BrittanyTinnin <bntinnin7@gmail.com>
2018-10-27 00:01:03 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/auth/facebook/callback' => 'sessions#create'
   root 'welcome#home'
 
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181027013501) do
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "uid"
+  end
 
 end
