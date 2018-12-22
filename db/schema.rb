9cfd36f67b64e5984d86c1e720c82d8dd8c26e7a
github username <github email address>
2018-12-22 14:35:50 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181222193134) do
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.integer  "uid"
+    t.string   "image"
+  end
 
 end
