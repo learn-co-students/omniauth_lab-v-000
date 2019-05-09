6aedd9c7c2015588bfd0ea0d219be6d03147f061
sarawall1187 <sarawall1187@gmail.com>
2019-05-09 09:57:32 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/auth/facebook/callback' => 'sessions#create'
+  get '/welcome/home' => 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
