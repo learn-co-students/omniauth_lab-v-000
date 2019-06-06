f876efcb19b14875b933ac984b94263b2d2c485c
AlwinaO <aoyewole2586@gmail.com>
2019-06-05 22:09:00 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
 
     user = User.find_or_create_by(uid: auth['uid']) do |user|
-      binding.pry
+      # binding.pry
       user.email = auth['info']['email']
       user.name = auth['info']['name']
     end
