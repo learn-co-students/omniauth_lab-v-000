3a0533cbcf9b98fe81ab52a709b9733706cbbfb1
Blake A. Long <blake.a.long@gmail.com>
2020-03-12 17:19:32 -0600

    ignore env

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # Add your routes here
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  post '/logout', to: 'sessions#destroy'
+
+  root 'welcome#home'
 end
