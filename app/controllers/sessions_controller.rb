d07935d6a0e3469defd9149cc6b424f656b102fa
github username <github email address>
2020-05-14 22:43:39 -0400

    Save the authentication hash user data in the session hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.image = auth['info']['image']
-    end
+    session[:name] = auth['info']['name']
+    session[:omniauth_data] = auth
 
-    session[:user_id] = @user.id
-
-    render 'welcome/home'
+    redirect_to root_path
   end
 
   private
