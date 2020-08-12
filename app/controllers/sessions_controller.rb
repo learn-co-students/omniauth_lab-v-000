6048d9fb2db33c323abaebd892e2f167fe427fc2
Sheena Bonilla <sheena.myb@gmail.com>
2020-08-12 23:44:10 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,6 @@ class SessionsController < ApplicationController
     session[:omniauth_data] = request.env['omniauth.auth']
 
     # Ye olde redirect
-    redirect_to root_path
+    redirect_to '/'
   end
 end
